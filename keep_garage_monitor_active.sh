ca55285e30ed64644c73812414120aeba4dda3f6
Brian Hanifin <code@brianhanifin.com>
2012-11-15 11:04:09 -0800

    Increase reliability by running a bash script every 5 minutes to verify the python script is still running. If it is not running, the service is started again.

--- keep_garage_monitor_active.sh
+++ keep_garage_monitor_active.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-##################
-# Run this script with cron every 5 minutes or so to keep the Garage Monitor running!
+################################################################################
+# Run this script with cron every 5 minutes to keep the Garage Monitor running!
 # Source: http://www.anyexample.com/linux_bsd/bash/check_if_program_is_running_with_bash_shell_script.xml
 # 
 # Run this script every 5 minutes in crontab (crontab -e)
 # */5 * * * * bash /usr/share/adafruit/webide/repositories/my-pi-projects/Garage_Monitor/keep_garage_monitor_active.sh
-###
+################################################################################
 
 # Define the name of the python script
 SERVICE='garagemonitor.py'
